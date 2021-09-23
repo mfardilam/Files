d63fccd5cd6fc7c0f073d193bf6b849f902a84a0
mfardilam <MariaA>
2021-09-23 18:22:05 -0500

    Inicial

---
+++ xxxx/src/main/java/com/example/xxxx/XxxxApplication.java
@@ -0,0 +1,13 @@
+package com.example.xxxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxxxApplication.class, args);
+	}
+
+}
---
+++ xxxx/src/test/java/com/example/xxxx/XxxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xxxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
